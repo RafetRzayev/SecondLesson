a7f0e97507f7a9bd8dfd51a8b3979abaa8f210f0
Rafet <rafetr@risk.az>
2024-04-30 20:37:06 +0400

    init

---
+++ SecondLesson/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SecondLesson
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a / b);
+
+            //byte c = 34;
+            //int d = c;
+
+            //int e = 300;
+            //byte f = (byte)e;
+
+            //string s = "hello\"";
+            //string s1 = "hello\t.";
+            //char ch = 'h';
+
+        }
+    }
+}
